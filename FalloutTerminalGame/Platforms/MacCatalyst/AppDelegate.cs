0ad3a2f29b700d30914c21be7f399b2f37528323
MultiTron <iliev_evn@hotmail.com>
2025-01-14 11:04:42 +0200

    Add project files.

---
+++ FalloutTerminalGame/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FalloutTerminalGame
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ FalloutTerminalGame/Resources/Utils/WordLinkeness.cs
@@ -0,0 +1,16 @@
+﻿namespace FalloutTerminalGame.Resources.Utils;
+public static class WordLinkeness
+{
+    public static int Calculate(string guess, string correct)
+    {
+        var likeness = 0;
+        for (int i = 0; i < guess.Length && i < correct.Length; i++)
+        {
+            if (guess[i] == correct[i])
+            {
+                likeness++;
+            }
+        }
+        return likeness;
+    }
+}
